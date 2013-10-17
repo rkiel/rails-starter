de858ddd3db9262e7f8d34899a39f5eafa05dd17
rkiel <cygnus@pobox.com>
2013-10-16 23:39:37 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-rails-starter
-=============
+# Rails Starter
 
-The output from running `rails new rails-starter`
+This is a starting point for a new Rails application.
+
+The output from running `rails new rails-starter` on October 16, 2013 has been captured without modification.
+
+The version of Rails used was 4.0.0.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsStarter::Application.config.secret_key_base = 'f12434754d25a51ae4e62f33721e20b00f27e2e4ec08720fc31e688204da54ac64120e8d4f2efe923ad79c63c8f84d8cb8faed2f933eda7191334da8ffb9230f'
