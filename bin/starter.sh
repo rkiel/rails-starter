7a66cf8aa3f759339911e8b42ae6bd54f81a033b
rkiel <cygnus@pobox.com>
2013-10-20 15:40:14 -0400

    scripted everything

--- bin/starter.sh
+++ bin/starter.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+echo "reseting README.md"
+echo "# $1" > README.md
+
+echo "creating sample database.yml"
+cp config/database.yml config/database.sample.yml
+rm config/database.yml
+echo "config/database.yml" >> .gitignore
+
+echo "changing application name to $1"
 sed -i .bak -e "s/RailsStarter/$1/" app/views/layouts/application.html.erb
 sed -i .bak -e "s/RailsStarter/$1/" config/application.rb
 sed -i .bak -e "s/RailsStarter/$1/" config/environment.rb
