fb7216de9cb607532d4f8cb552d46f3b43cc0b54
rkiel <cygnus@pobox.com>
2014-05-09 16:26:56 -0400

    rails 4.1.1

--- bin/starter.sh
+++ bin/starter.sh
@@ -10,14 +10,6 @@ echo "config/database.yml" >> .gitignore
 echo "changing application name to $1"
 sed -i .bak -e "s/RailsStarter/$1/" app/views/layouts/application.html.erb
 sed -i .bak -e "s/RailsStarter/$1/" config/application.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/environment.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/environments/development.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/environments/production.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/environments/test.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/initializers/secret_token.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/initializers/session_store.rb
-sed -i .bak -e "s/RailsStarter/$1/" config/routes.rb
-sed -i .bak -e "s/RailsStarter/$1/" Rakefile
 
 sed -i .bak -e "s/rails-starter/$1/" config/initializers/session_store.rb
 
